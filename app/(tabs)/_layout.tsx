91454b39608bed39a27c98fdab8971dd656fc644
Jeremy Polanco Lara <jeremyinf26@gmail.com>
2023-10-25 18:50:33 -0400

    add custom tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,11 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs } from "expo-router";
-import { Pressable, useColorScheme } from "react-native";
+import { useColorScheme } from "react-native";
 
 import Colors from "../../constants/Colors";
+
+import styled from "styled-components/native";
+import { useMemo } from "react";
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>["name"];
@@ -19,52 +22,162 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
       }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Inicio",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Emergencia",
-          tabBarIcon: ({ color }) => <TabBarIcon name="hospital-o" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-
-        name="[id]"
-        options={{
-          title: "Pantalla",
-          tabBarIcon: ({ color }) => <TabBarIcon name="ambulance" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-
-        name="form"
-        options={{
-          title: "Formulario",
-          tabBarIcon: ({ color }) => <TabBarIcon name="wpforms" color={color} />,
-        }}
-      />
-     
-    </Tabs>
+      tabBar={(props) => <TabBar {...props} />}
+    ></Tabs>
   );
 }
+
+const TabBar = ({ navigation }: any) => {
+  const state = navigation.getState();
+  const actualRoute = state.routes[state.index].name;
+
+  const routes = useMemo(() => {
+    return [
+      { id: 1, name: "index" as any },
+      { id: 2, name: "home" as any },
+      { id: 3, name: "vista" as any },
+      { id: 4, name: "form" as any },
+    ];
+  }, []);
+
+  return (
+    <TabBarWrapper>
+      {routes.map(({ name, id }) => {
+        let iconName: string = "";
+
+        let focused = actualRoute === name;
+
+        const color = focused ? "#222" : "#fff";
+
+        if (name.includes("/")) {
+          focused = name.includes(actualRoute.split(" ")[0]);
+        }
+
+        switch (name) {
+          case "index":
+            iconName = "home";
+            break;
+
+          case "home":
+            iconName = "hospital-o";
+            break;
+
+          case "vista":
+            iconName = "ambulance";
+            break;
+
+          case "form":
+            iconName = "wpforms";
+            break;
+        }
+
+        if (iconName === "home") {
+          return (
+            <Link href={"/(tabs)"} asChild key={id}>
+              <TabBarItem>
+                <TabBarIconAndLabelWrapper>
+                  <TabBarIcon name={iconName} color={color} />
+                  <TabBarLabelText
+                    color={color}
+                    fontWeight={focused ? "500" : "normal"}
+                  >
+                    Inicio
+                  </TabBarLabelText>
+                </TabBarIconAndLabelWrapper>
+              </TabBarItem>
+            </Link>
+          );
+        } else if (iconName === "hospital-o") {
+          return (
+            <Link href={name} asChild key={id}>
+              <TabBarItem>
+                <TabBarIconAndLabelWrapper>
+                  <TabBarIcon name={iconName} color={color} />
+                  <TabBarLabelText
+                    color={color}
+                    fontWeight={focused ? "500" : "normal"}
+                  >
+                    Emergencia
+                  </TabBarLabelText>
+                </TabBarIconAndLabelWrapper>
+              </TabBarItem>
+            </Link>
+          );
+        } else if (iconName === "ambulance") {
+          return (
+            <Link href={name} asChild key={id}>
+              <TabBarItem>
+                <TabBarIconAndLabelWrapper>
+                  <TabBarIcon name={iconName} color={color} />
+                  <TabBarLabelText
+                    color={color}
+                    fontWeight={focused ? "500" : "normal"}
+                  >
+                    Pantalla
+                  </TabBarLabelText>
+                </TabBarIconAndLabelWrapper>
+              </TabBarItem>
+            </Link>
+          );
+        } else if (iconName === "wpforms") {
+          return (
+            <Link href={name} asChild key={id}>
+              <TabBarItem>
+                <TabBarIconAndLabelWrapper>
+                  <TabBarIcon name={iconName} color={color} />
+                  <TabBarLabelText
+                    color={focused ? "#222" : "#fff"}
+                    fontWeight={focused ? "500" : "normal"}
+                  >
+                    Formulario
+                  </TabBarLabelText>
+                </TabBarIconAndLabelWrapper>
+              </TabBarItem>
+            </Link>
+          );
+        }
+      })}
+    </TabBarWrapper>
+  );
+};
+
+const TabBarWrapper = styled.SafeAreaView`
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: null;
+  transform: translate(0);
+  background: #dcdcdc;
+  height: 56px;
+  border-top: 1px solid black;
+  padding-left: 0;
+  padding-right: 0;
+  padding-bottom: 0;
+  flex-direction: row;
+`;
+
+const TabBarItem = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-end;
+  background-color: transparent;
+`;
+
+const TabBarIconAndLabelWrapper = styled.View`
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  min-width: 25px;
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TabBarLabelText = styled.Text<{ color: string; fontWeight: string }>`
+  color: ${(props) => props.color};
+  font-size: 11px;
+  font-weight: ${(props) => props.fontWeight};
+`;
---
+++ app/(tabs)/vista.tsx
@@ -0,0 +1,84 @@
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+} from "react-native";
+
+const TabFourScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Formulario de registro</Text>
+      <View style={styles.formContainer}>
+        <TextInput style={styles.input} placeholder="Nombre" />
+        <TextInput
+          style={styles.input}
+          placeholder="Edad"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cédula"
+          keyboardType="numeric"
+        />
+        <TextInput style={styles.input} placeholder="Tipo sangre" />
+        <TextInput style={styles.input} placeholder="Enfermedades que padece" />
+
+        <TextInput style={styles.input} placeholder="Alergias" />
+        <TextInput
+          style={styles.input}
+          placeholder="Primer contacto de emergencia"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Segundo contacto de emergencia"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Numero de residencia"
+          keyboardType="numeric"
+        />
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor="#3e8651ff"
+          onPress={() => {}}
+        >
+          <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  formContainer: { marginHorizontal: 40, justifyContent: "center", flex: 1 },
+  title: {
+    top: 80,
+    fontWeight: "bold",
+    fontSize: 25,
+    alignSelf: "center",
+  },
+  input: {
+    height: 60,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  button: {
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: "#56be70",
+    borderRadius: 12,
+  },
+  buttonText: {
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
+});
+
+export default TabFourScreen;
